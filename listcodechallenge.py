d5084816f1917c4d9c7b263a44d170e1a011cfe8
Jason Zaitz <jasonzaitz1@gmail.com>
2018-10-08 13:10:32 -0400

    added new code challenge

--- listcodechallenge.py
+++ listcodechallenge.py
@@ -5,3 +5,33 @@ def first_last6(nums):
     return True
   else:
     return False
+
+
+#Given an array of ints, return True if the array is length 1 or more, and the first element and the last element are equal.
+
+def same_first_last(nums):
+  if len(nums) >= 1 and nums[0] == nums[-1]:
+    return True
+  else:
+    return False
+
+#Return an int array length 3 containing the first 3 digits of pi, {3, 1, 4}.
+def make_pi():
+  return [3,1,4]
+
+
+#Given 2 arrays of ints, a and b, return True if they have the same first element or they have the same last element. Both arrays will be length 1 or more.
+def common_end(a, b):
+  if a[0] == b[0] or a[-1] == b[-1]:
+    return True
+  else:
+    return False
+
+#Given an array of ints length 3, return the sum of all the elements.
+def sum3(nums):
+  return nums[0] + nums[1] + nums[2]
+
+#Given an array of ints length 3, return an array with the elements "rotated left" so {1, 2, 3} yields {2, 3, 1}.
+def rotate_left3(nums):
+  new = [nums[1], nums[2], nums[0]]
+  return new
