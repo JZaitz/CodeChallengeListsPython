002f440e0fc7e437a029c419fae40690517aed36
Jason Zaitz <jasonzaitz1@gmail.com>
2018-10-08 13:37:02 -0400

    added new code challenge

--- listcodechallenge.py
+++ listcodechallenge.py
@@ -35,3 +35,50 @@ def sum3(nums):
 def rotate_left3(nums):
   new = [nums[1], nums[2], nums[0]]
   return new
+
+#Given an array of ints length 3, return a new array with the elements in reverse order, so {1, 2, 3} becomes {3, 2, 1}.
+def reverse3(nums):
+  return [nums[2], nums[1], nums[0]]
+
+#Given an array of ints length 3, figure out which is larger, the first or last element in the array, and set all the other elements to be that value. Return the changed array.
+
+def max_end3(nums):
+  if nums[0] >= nums[-1]:
+    return [nums[0], nums[0], nums[0]]
+  elif nums[-1] > nums[0]:
+    return [nums[-1], nums[-1], nums[-1]]
+  else:
+    return nums
+
+#Given an array of ints, return the sum of the first 2 elements in the array. If the array length is less than 2, just sum up the elements that exist, returning 0 if the array is length 0.
+
+def sum2(nums):
+  if len(nums) >= 2:
+    return nums[0] + nums[1]
+  elif len(nums) == 1:
+    return nums[0]
+  else:
+    return 0
+
+#Given 2 int arrays, a and b, each length 3, return a new array length 2 containing their middle elements.
+def middle_way(a, b):
+  newarray = [a[1], b[1]]
+  return newarray
+
+#Given an array of ints, return a new array length 2 containing the first and last elements from the original array. The original array will be length 1 or more.
+def make_ends(nums):
+  if len(nums) == 1:
+    return [nums[0], nums[0]]
+  elif len(nums) >= 2:
+    newarray = [nums[0], nums[-1]]
+    return newarray
+
+
+#Given an int array length 2, return True if it contains a 2 or a 3.
+def has23(nums):
+  if nums[0] == 2 or nums[0] == 3:
+    return True
+  elif nums[1] == 2 or nums[1] == 3:
+    return True
+  else:
+    return False
