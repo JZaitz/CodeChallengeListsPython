43005d32825f600bf6e862c858605fa66208fab7
Jason Zaitz <jasonzaitz1@gmail.com>
2018-10-09 12:35:45 -0400

    added new code challenge

--- listcodechallenge.py
+++ listcodechallenge.py
@@ -91,3 +91,22 @@ def cigar_party(cigars, is_weekend):
     return True
   else:
     return False
+
+#You and your date are trying to get a table at a restaurant. The parameter "you" is the stylishness of your clothes, in the range 0..10, and "date" is the stylishness of your date's clothes. The result getting the table is encoded as an int value with 0=no, 1=maybe, 2=yes. If either of you is very stylish, 8 or more, then the result is 2 (yes). With the exception that if either of you has style of 2 or less, then the result is 0 (no). Otherwise the result is 1 (maybe).
+def date_fashion(you, date):
+  if you <= 2 or date <= 2:
+    return 0
+  if you >= 8 or date >= 8:
+    return 2
+  else:
+    return 1
+
+
+#The squirrels in Palo Alto spend most of the day playing. In particular, they play if the temperature is between 60 and 90 (inclusive). Unless it is summer, then the upper limit is 100 instead of 90. Given an int temperature and a boolean is_summer, return True if the squirrels play and False otherwise.
+def squirrel_play(temp, is_summer):
+  if temp >= 60 and temp <= 90 and not is_summer:
+    return True
+  elif is_summer and temp >= 60 and temp <= 100:
+    return True
+  else:
+    return False
