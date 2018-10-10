eea9aad0b4dab773b16c8695ce06616505236443
Jason Zaitz <jasonzaitz1@gmail.com>
2018-10-10 18:08:58 -0400

    added new code challenge

--- listcodechallenge.py
+++ listcodechallenge.py
@@ -146,4 +146,17 @@ def alarm_clock(day, vacation):
       return "10:00"
     else:
       return "off"
-      
+
+
+#The number 6 is a truly great number. Given two int values, a and b, return True if either one is 6. Or if their sum or difference is 6. Note: the function abs(num) computes the absolute value of a number.
+def love6(a, b):
+  if a + b == 6:
+    return True
+  elif a - b == 6:
+    return True
+  elif b - a == 6:
+    return True
+  elif a == 6 or b == 6:
+    return True
+  else:
+    return False
