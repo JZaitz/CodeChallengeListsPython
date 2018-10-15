c6c40a4d902240affcafb10534d59d54808987e2
Jason Zaitz <jasonzaitz1@gmail.com>
2018-10-15 12:40:10 -0400

    added new code challenge

--- listcodechallenge.py
+++ listcodechallenge.py
@@ -174,3 +174,12 @@ def in1to10(n, outside_mode):
       return True
     else:
       return False
+
+#Given a non-negative number "num", return True if num is within 2 of a multiple of 10. Note: (a % b) is the remainder of dividing a by b, so (7 % 5) is 2. See also: Introduction to Mod
+def near_ten(num):
+  if num % 10 <= 2:
+    return True
+  elif num % 10 >= 8 and num % 10 <= 10:
+    return True
+  else:
+    return False
