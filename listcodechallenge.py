c74d405caff8981af56639f94d8274b37b9ca29e
Jason Zaitz <jasonzaitz1@gmail.com>
2018-10-17 18:19:46 -0400

    added new code challenge

--- listcodechallenge.py
+++ listcodechallenge.py
@@ -245,4 +245,11 @@ def round10(n):
     y = (n % 10)
     n = n - y
     return n
-  
+
+
+#Given three ints, a b c, return True if one of b or c is "close" (differing from a by at most 1), while the other is "far", differing from both other values by 2 or more. Note: abs(num) computes the absolute value of a number.
+def close_far(a, b, c):
+  if (abs(a - b) <= 1 and abs(a - c) >= 2) or (abs(a - c) <= 1 and abs(a - b) >= 2):
+    return True
+  else:
+    return False
