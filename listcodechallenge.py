dae187f4663248b332bd0213ed50026ed38cd618
Jason Zaitz <jasonzaitz1@gmail.com>
2018-10-22 13:33:47 -0400

    added new code challenge

--- listcodechallenge.py
+++ listcodechallenge.py
@@ -212,19 +212,19 @@ def lucky_sum(a, b, c):
 #Given 3 int values, a b c, return their sum. However, if any of the values is a teen -- in the range 13..19 inclusive -- then that value counts as 0, except 15 and 16 do not count as a teens. Write a separate helper "def fix_teen(n):"that takes in an int value and returns that value fixed for the teen rule. In this way, you avoid repeating the teen code 3 times (i.e. "decomposition"). Define the helper below and at the same indent level as the main no_teen_sum().
 
 def no_teen_sum(a, b, c):
-  return fix_teen(a)
-  return fix_teen(b)
-  return ix_teen(c)
-  sum = a + b + c
+  fix_teen(a)
+  sum = fix_teen(a) + fix_teen(b) + fix_teen(c)
   return sum
 
 
-def fix_teen(x):
-  if x == 13 or x == 14:
-    x = 0
-  elif x == 17 or x == 18 or x == 19:
-    x = 0
-  return x
+
+def fix_teen(n):
+  if n == 13 or n == 14 or n == 17 or n ==18 or n == 19:
+    n = 0
+  else:
+    n = n
+  return n
+
 
 #For this problem, we'll round an int value up to the next multiple of 10 if its rightmost digit is 5 or more, so 15 rounds up to 20. Alternately, round down to the previous multiple of 10 if its rightmost digit is less than 5, so 12 rounds down to 10. Given 3 ints, a b c, return the sum of their rounded values. To avoid code repetition, write a separate helper "def round10(num):" and call it 3 times. Write the helper entirely below and at the same indent level as round_sum().
 def round_sum(a, b, c):
@@ -283,7 +283,7 @@ def make_bricks(small, big, goal):
   else:
     return False
 
-    
+
 #Given 3 int values, a b c, return their sum. However, if one of the values is the same as another of the values, it does not count towards the sum.
 def lone_sum(a, b, c):
   if a != b and b != c and a != c:
