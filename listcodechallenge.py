b0b7887e2058526f86703681c6abdbad252b994b
Jason Zaitz <jasonzaitz1@gmail.com>
2018-10-24 14:43:52 -0400

    added new code challenge

--- listcodechallenge.py
+++ listcodechallenge.py
@@ -311,8 +311,6 @@ def count_hi(str):
       counter = counter + 1
     elif str[i:i+2] == "hi":
       counter = counter + 1
-    else:
-      return 0
   return counter
 
  #Return True if the string "cat" and "dog" appear the same number of times in the given string.
@@ -328,3 +326,13 @@ def count_hi(str):
     return True
   elif ccount != dcount:
     return False
+
+#Return the number of times that the string "code" appears anywhere in the given string, except we'll accept any letter for the 'd', so "cope" and "cooe" count.
+def count_code(str):
+  counter = 0
+  for i in range(0, len(str) - 3):
+    if str[i:i+2] == "co" and str[i+3] == "e":
+      counter = counter + 1
+    elif str[i] == "c" and str[i+1] == "o" and str[i+3] == "e":
+      counter = counter + 1
+  return counter
