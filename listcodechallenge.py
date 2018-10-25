34ca0cfc1f3f4c17388ac3fe09b063573af904f8
Jason Zaitz <jasonzaitz1@gmail.com>
2018-10-25 14:50:53 -0400

    added Ruby translation exercise

--- listcodechallenge.py
+++ listcodechallenge.py
@@ -357,6 +357,27 @@ end
   elif ccount != dcount:
     return False
 
+"""RUBY TRANSLATION
+def cat_dog(str)
+  ccount = 0
+  dcount = 0
+  for i in 0..str.length-4
+    if str[i..i+2] == "cat"
+      ccount += 1
+    elsif str[i:i+2] == "dog"
+      dcount += 1
+    end 
+    if ccount == dcount
+      return True
+    elsif ccount != dcount
+      return False
+    end
+  end
+
+end
+
+"""
+
 #Return the number of times that the string "code" appears anywhere in the given string, except we'll accept any letter for the 'd', so "cope" and "cooe" count.
 def count_code(str):
   counter = 0
