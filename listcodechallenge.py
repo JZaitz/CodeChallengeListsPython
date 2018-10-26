ac617c5ff16ec20a7491b227979474b673fe8ea7
Jason Zaitz <jasonzaitz1@gmail.com>
2018-10-26 12:46:04 -0400

    added new code challenge

--- listcodechallenge.py
+++ listcodechallenge.py
@@ -492,3 +492,22 @@ def big_diff(nums):
    return answer
  end
  """
+
+ #Return the "centered" average of an array of ints, which we'll say is the mean average of the values, except ignoring the largest and smallest values in the array. If there are multiple copies of the smallest value, ignore just one copy, and likewise for the largest value. Use int division to produce the final average. You may assume that the array is length 3 or more.
+def centered_average(nums):
+  nums.sort()
+  newnums = nums[1:-1]
+  summ = sum(newnums)
+  answer = summ/len(newnums)
+  return answer
+
+ """Ruby
+def centered_average(nums)
+  nums.sort
+  newnums = nums[1:-2]
+  summ = newnums.reduce(:+)
+  answer summ/newnums.length
+  return answer 
+end
+
+ """
